1b303f656ec49c77ee1b622e0f1e2b0372f94736
Arkadiusz Plichta <el.pikel@gmail.com>
2017-07-01 09:20:22 +0200

    test refactor

--- test/cache_test.exs
+++ test/cache_test.exs
@@ -2,37 +2,35 @@ defmodule CacheTest do
   use ExUnit.Case
   alias Tleaog.Cache
 
-  test "can store any valid Elixir term, given a key" do
+  @stooges ["Lary", "Curly", "Moe"]
+
+  setup do
     Cache.start_link
-    stooges = ["Lary", "Curly", "Moe"]
-    Cache.write(:stooges, stooges)
-    assert Cache.read(:stooges) == stooges
+    Cache.write(:stooges, @stooges)
+
+    :ok
+  end
+
+  test "can store any valid Elixir term, given a key" do
+    assert Cache.read(:stooges) == @stooges
+  end
+
+  test "updates when key already exists" do
+    Cache.write(:stooges, :new)
+    assert Cache.read(:stooges) == :new
   end
 
   test "can delete" do
-    Cache.start_link
-    stooges = ["Lary", "Curly", "Moe"]
-    Cache.write(:stooges, stooges)
-    assert Cache.read(:stooges) == stooges
-
     Cache.delete(:stooges)
     assert Cache.read(:stooges) == nil
   end
 
   test "can clear" do
-    Cache.start_link
-    stooges = ["Lary", "Curly", "Moe"]
-    Cache.write(:stooges, stooges)
-    assert Cache.read(:stooges) == stooges
-
     Cache.clear()
     assert Cache.read(:stooges) == nil
   end
 
   test "can check if exists" do
-    Cache.start_link
-    stooges = ["Lary", "Curly", "Moe"]
-    Cache.write(:stooges, stooges)
     assert Cache.exists?(:stooges) == true
   end
 end
